dd54549b3e4d63dddff6d2e20d590618172d8d7b
ygorjg <ygorjgr@gmail.com>
2023-02-10 21:40:08 -0300

    feat: add card style & finally project

--- src/components/Form/PokeForm.vue
+++ src/components/Form/PokeForm.vue
@@ -1,21 +1,144 @@
 <template>
-  <div>
-    <label>Insira o nome do pokémon que deseja buscar:</label>
+  <main>
     <form id="poke-form">
-      <input
-        type="text"
-        id="name"
-        name="name"
-        v-model="name"
-        placeholder="Digite aqui"
-      />
-      <button type="submit">BUSCAR</button>
+      <div class="label-container">
+        <h4>Insira o nome do pokémon que deseja buscar:</h4>
+      </div>
+      <div class="input-container">
+        <input
+          type="text"
+          id="name"
+          name="name"
+          v-model="name"
+          placeholder="Digite aqui"
+        />
+        <input
+          class="btn-submit"
+          type="submit"
+          value="BUSCAR"
+          v-on:click="submitForm"
+        />
+      </div>
     </form>
-  </div>
+    <div class="card-container">
+      <div class="card">
+        <img :src="pokemon.img" alt="" />
+        <p>{{ pokemon.name }}</p>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "PokeForm",
+  data() {
+    return {
+      form: {
+        name: "",
+      },
+      pokemon: {
+        name: "",
+        img: "",
+      },
+    };
+  },
+  methods: {
+    submitForm(e) {
+      e.preventDefault();
+      axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${this.name}`)
+        .then((response) => {
+          console.log(response.data);
+          this.pokemon.name = response.data.name;
+          this.pokemon.img =
+            response.data.sprites.other.dream_world.front_default;
+        });
+    },
+  },
 };
 </script>
+
+<style scoped lang="scss">
+main {
+  widows: 100vw;
+  height: 100vh;
+  background-color: #1eb1df;
+}
+.label-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+}
+
+h4 {
+  font-family: "Lexend Exa";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 25px;
+  color: #edff2c;
+  text-align: center;
+  width: 250px;
+  margin-top: 10px;
+}
+.input-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60px;
+}
+
+#name {
+  background: #dcdfb7;
+  border-radius: 10px 0px 0px 10px;
+  border: none;
+  width: 180px;
+  height: 30px;
+}
+.btn-submit {
+  font-family: "Lexend Exa";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 19px;
+  border: none;
+  text-align: center;
+  color: #e2f03e;
+  background: #5c5e41;
+  border-radius: 0px 10px 10px 0px;
+  height: 30px;
+  width: 80px;
+}
+.btn-submit:hover {
+  cursor: pointer;
+  transition: 200ms;
+  background-color: #e2f03e;
+  color: #5c5e41;
+}
+.card-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+}
+
+.card {
+  background-color:  #5c5e41;
+  text-align: center;
+}
+
+p {
+  font-family: "Lexend Exa";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 20px;
+  line-height: 25px;
+  color: #edff2c;
+  text-align: center;
+  width: 250px;
+  margin-top: 5px;
+}
+</style>
--- src/components/HeaderPage/HeaderPage.vue
+++ src/components/HeaderPage/HeaderPage.vue
@@ -12,15 +12,24 @@ export default {
 
 <style scoped lang="scss">
 header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 50px;
   background: var(--color-background-header);
 }
 
 h1 {
+  font-family: "Lexend Exa";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 38px;
+  line-height: 48px;
   color: var(--color-yellow);
 }
 
 span {
+  font-size: 25px;
   color: #fff;
 }
 </style>
